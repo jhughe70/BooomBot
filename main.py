7e983e31a167d1cc099538c0a75ca4026003f727
Jared <jaredahughes01@gmail.com>
2021-09-17 20:21:45 -0400

    Made API key secure
    Created a variable for the API key to be posted online

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import booomBot
+import os
+import Constants
 
 
 cogs = [booomBot]
@@ -10,4 +12,10 @@ client = commands.Bot(command_prefix="?", intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("ODg4NDkyNTg0NjE0Mzc5NTUy.YUTfMw.biUtiR8ZUDfCnkGyBjGzsNQwt6o")
+key = 'booomBot_key'
+#print(os.environ)
+print(Constants.booomBot_key)
+#print(os.getenv(key))
+
+
+#client.run(os.getenv(key))
